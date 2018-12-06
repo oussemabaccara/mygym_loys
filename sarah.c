eef3355167d0cf327b52716c54a1a05e60937d7a
BrahimDriss <brahim.driss@esprit.tn>
2018-12-06 14:26:27 +0100

    Ceci est une fonction pour sarah veuillez ne pas l'utiliser si vous n'etes pas sarah ou vous ne faites pas partie du groupe loys

--- sarah.c
+++ sarah.c
@@ -1,1 +1,41 @@
+void intstring(int x,char su[])
+{
+
+    switch(x) {
+
+   case 1  : strcpy(su,"1");break;
+   case 2  : strcpy(su,"2");break;
+   case 3  : strcpy(su,"3");break;
+   case 4  : strcpy(su,"4");break;
+   case 5  : strcpy(su,"5");break;
+   case 6  : strcpy(su,"6");break;
+   case 7  : strcpy(su,"7");break;
+   case 8  : strcpy(su,"8");break;
+   case 9  : strcpy(su,"9");break;
+   case 10 : strcpy(su,"10");break;
+   case 11 : strcpy(su,"11");break;
+   case 12  : strcpy(su,"12");break;
+   case 13  : strcpy(su,"13");break;
+   case 14  : strcpy(su,"14");break;
+   case 15  : strcpy(su,"15");break;
+   case 16  : strcpy(su,"16");break;
+   case 17  : strcpy(su,"17");break;
+   case 18  : strcpy(su,"18");break;
+   case 19  : strcpy(su,"19");break;
+   case 20  : strcpy(su,"20");break;
+   case 21  : strcpy(su,"21");break;
+   case 22  : strcpy(su,"22");break;
+   case 23  : strcpy(su,"23");break;
+   case 24  : strcpy(su,"24");break;
+   case 25  : strcpy(su,"25");break;
+   case 26  : strcpy(su,"26");break;
+   case 27  : strcpy(su,"27");break;
+   case 28  : strcpy(su,"28");break;
+   case 29  : strcpy(su,"29");break;
+   case 30  : strcpy(su,"30");break;
+   case 31  : strcpy(su,"31");break;
+    default :   return(0);
+}
+}
+
  
