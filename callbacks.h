a95b25df9e708f5cd60636b4efd952ef1c9829f5
yasminechaabene <yasminechaabene@esprit.tn>
2018-12-09 04:57:09 -0800

    projet10

---
+++ callbacks.h
@@ -0,0 +1,43 @@
+#include <gtk/gtk.h>
+
+
+
+
+void
+on_button1_clicked                     (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+
+void
+on_button3_clicked                     (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button5_clicked                     (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+
+
+
+
+
+
+void
+on_button12_clicked                    (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button13_clicked                    (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button14_clicked                    (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button10_clicked                    (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button11_clicked                    (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
