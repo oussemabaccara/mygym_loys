30dc6bc58c964b974cb0f8cf82688e1b33d1727c
yasminechaabene <yasminechaabene@esprit.tn>
2018-12-12 13:17:52 -0800

    finallllllll

---
+++ projet_avec_modif_finall/project12/src/callbacks.h
@@ -0,0 +1,81 @@
+#include <gtk/gtk.h>
+
+
+void
+on_button2_clicked                     (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button1_clicked                     (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button4_clicked                     (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button3_clicked                     (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button5_clicked                     (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button6_clicked                     (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button8_clicked                     (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button7_clicked                     (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button9_clicked                     (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button10_clicked                    (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+
+void
+on_button12_clicked                    (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button13_clicked                    (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button14_clicked                    (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button15_clicked                    (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+
+
+
+void
+on_button16_clicked                    (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+
+
+void
+on_button19_clicked                    (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+void
+on_button18_clicked                    (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
+
+
+void
+on_button17_clicked                    (GtkWidget       *objet_graphique,
+                                        gpointer         user_data);
