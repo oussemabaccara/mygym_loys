2a2717de2b699159450022ccdbf810c41a853a32
oussemabaccara <oussema.baccara@esprit.tn>
2018-12-11 17:05:48 -0800

    a integre

---
+++ projet3/src/event.h
@@ -0,0 +1,23 @@
+
+#include <gtk/gtk.h>
+
+typedef struct
+{	char num[5];
+	char nom[40];
+	char description[100];
+	char date[10];
+	char heure[10];
+	char nombre[5];
+	char nombremax[5];
+}event;
+
+void afficher_event(GtkWidget *liste);
+int ajout_part(char num[],char id[]);
+int modif_nb(char num[]);
+int inscription(char id[],char num[]);
+int modif_nb1(char num[]);
+void supression_part(char num[]);
+int annulation(char id[],char num[]);
+
+
+
